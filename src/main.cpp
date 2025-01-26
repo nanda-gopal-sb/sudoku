0017900c80f9e8f68112e655dc5a0ae468b54139
Roopesh O R <62459820+Roopesh2@users.noreply.github.com>
2025-01-26 19:17:45 +0530

    Refactored code and added extra keycodes for input
    Signed-off-by: Roopesh O R <62459820+Roopesh2@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,33 @@
 
 #define CELL_SIZE 80
 #define offset 240
+
+typedef sf::Keyboard::Scancode Kcode;
+
 sf::Texture texture("assests/bg.png");
 sf::Font font("assests/type.ttf");
 sf::Text text(font);
 std::vector<Cell> cells;
 sf::Time elapsed1;
-sf::Image image("assests/sudoku.png");
+sf::Image windowIcon("assests/sudoku.png");
 sf::Texture drawing("assests/drawing.png");
 sf::Texture notDrawing("assests/not-drawing.png");
 sf::Sprite pencilIcon(notDrawing);
+
+// for handling keypress events
+// handle both normal key and numberpad key
+Kcode numcodes[18] = {
+  Kcode::Num1, Kcode::Numpad1,
+  Kcode::Num2, Kcode::Numpad2,
+  Kcode::Num3, Kcode::Numpad3,
+  Kcode::Num4, Kcode::Numpad4,
+  Kcode::Num5, Kcode::Numpad5,
+  Kcode::Num6, Kcode::Numpad6,
+  Kcode::Num7, Kcode::Numpad7,
+  Kcode::Num8, Kcode::Numpad8,
+  Kcode::Num9, Kcode::Numpad9
+};
+
 void sudokuGenerator(int k) {
   Sudoku *suk = new Sudoku();
   suk->fillDiagonal(cells);
@@ -67,11 +85,11 @@ void fillCell() {
 }
 
 /**
- * Returns 1D cell index from mouse's relative x y position
+ * Returns 1D cell index from mouse's relative (x,y) position
  */
-int getMousPos(sf::RenderWindow &window) {
-  int mouse_x = sf::Mouse::getPosition(window).x / CELL_SIZE;
-  int mouse_y = sf::Mouse::getPosition(window).y / CELL_SIZE;
+int getGridIndex(int x, int y) {
+  int mouse_x = x / CELL_SIZE;
+  int mouse_y = y / CELL_SIZE;
   mouse_y = mouse_y - offset/CELL_SIZE;
   if (mouse_y < 0) {
     return 0;
@@ -80,6 +98,10 @@ int getMousPos(sf::RenderWindow &window) {
     cells[mouse_x + mouse_y * 9].isSelected = true;
   return (mouse_x + mouse_y * 9);
 }
+
+/**
+ * Highlights a given number throughout the grid
+ */
 void highlight(int num) {
   for (int i = 0; i < 81; i++) {
     if (cells[i].number == num)
@@ -99,122 +121,118 @@ void clearHighlight() {
 /**
  * Shows initial play game screen
  */
-void startScreen(bool startedPlaying, sf::RenderWindow &window) {
-  if (!startedPlaying) {
-    text.setCharacterSize(30);
-    text.setFillColor(sf::Color::Cyan);
-    text.setString("PLAY");
-    text.setPosition({300, 100});
-    window.draw(text);
-  }
+void startScreen(sf::RenderWindow &window) {
+  text.setCharacterSize(30);
+  text.setFillColor(sf::Color::Cyan);
+  text.setString("PLAY");
+  text.setPosition({300, 100});
+  window.draw(text);
 }
 
 /**
  * Draw board
  */
-void drawRectangles(sf::RenderWindow &window, bool isWin, bool isStarted) {
-  if (isStarted) {
-    if (isWin) {
-      window.clear();
-      int timeTaken = (elapsed1.asSeconds());
-      text.setFillColor(sf::Color::White);
-      text.setPosition({200, 300});
-      text.setString("YOU WIN!\n" + std::to_string(timeTaken) + " seconds");
-      window.draw(text);
-      return;
-    }
+void drawRectangles(sf::RenderWindow &window, bool isWin) {
+  if (isWin) {
+    window.clear();
+    int timeTaken = (elapsed1.asSeconds());
+    text.setFillColor(sf::Color::White);
+    text.setPosition({200, 300});
+    text.setString("YOU WIN!\n" + std::to_string(timeTaken) + " seconds");
+    window.draw(text);
+    return;
+  }
 
-    sf::RectangleShape rectangle({CELL_SIZE, CELL_SIZE});
-    sf::RectangleShape subgridSeparator({5, 1100});
-    subgridSeparator.setFillColor(sf::Color::Black);
+  sf::RectangleShape rectangle({CELL_SIZE, CELL_SIZE});
+  sf::RectangleShape subgridSeparator({5, 1100});
+  subgridSeparator.setFillColor(sf::Color::Black);
 
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
 
-        if (cells[i + j * 9].isSelected) {
-          rectangle.setFillColor(sf::Color(99, 99, 99));
-        } else if (cells[i + j * 9].isHighlight) {
-          rectangle.setFillColor(sf::Color::Magenta);
-        } else {
-          rectangle.setFillColor(sf::Color::White);
-        }
+      if (cells[i + j * 9].isSelected) {
+        rectangle.setFillColor(sf::Color(99, 99, 99));
+      } else if (cells[i + j * 9].isHighlight) {
+        rectangle.setFillColor(sf::Color::Magenta);
+      } else {
+        rectangle.setFillColor(sf::Color::White);
+      }
 
-        rectangle.setOutlineThickness(1);
-        rectangle.setOutlineColor(sf::Color::Black);
-        rectangle.setPosition(
-            {float(CELL_SIZE * i), float(CELL_SIZE * j) + offset});
-        rectangle.setTexture(&texture);
-        window.draw(rectangle);
+      rectangle.setOutlineThickness(1);
+      rectangle.setOutlineColor(sf::Color::Black);
+      rectangle.setPosition(
+          {float(CELL_SIZE * i), float(CELL_SIZE * j) + offset});
+      rectangle.setTexture(&texture);
+      window.draw(rectangle);
 
-				// draw inner text
-        text.setPosition({float(CELL_SIZE * i), float(CELL_SIZE * j) + offset});
-        if (cells[i + j * 9].canBeChanged && !cells[i + j * 9].isPencil &&
-            cells[i + j * 9].number != 0) {
-          text.setCharacterSize(64);
-          text.setFillColor(sf::Color::Black);
-          std::string String = std::to_string(cells[i + j * 9].number);
-          String = " " + String;
-          text.setString(String);
-          window.draw(text);
-        } else if (cells[i + j * 9].isPencil) {
-          text.setCharacterSize(21);
-          text.setFillColor(sf::Color::Black);
-          text.setString(cells[i + j * 9].pencilNums);
-          window.draw(text);
-        } else if (cells[i + j * 9].number != 0) {
-          std::string String = std::to_string(cells[i + j * 9].number);
-          String = " " + String;
-          text.setString(String);
-          text.setCharacterSize(64);
-          text.setFillColor(sf::Color(99, 62, 21));
-          window.draw(text);
-        } else {
-          // random
-        }
+      // draw cell text
+      text.setPosition({float(CELL_SIZE * i), float(CELL_SIZE * j) + offset});
+      if (cells[i + j * 9].canBeChanged && !cells[i + j * 9].isPencil &&
+          cells[i + j * 9].number != 0) {
+        text.setCharacterSize(64);
+        text.setFillColor(sf::Color::Black);
+        std::string String = std::to_string(cells[i + j * 9].number);
+        String = " " + String;
+        text.setString(String);
+        window.draw(text);
+      } else if (cells[i + j * 9].isPencil) {
+        text.setCharacterSize(21);
+        text.setFillColor(sf::Color::Black);
+        text.setString(cells[i + j * 9].pencilNums);
+        window.draw(text);
+      } else if (cells[i + j * 9].number != 0) {
+        text.setCharacterSize(64);
+        text.setFillColor(sf::Color(99, 62, 21));
+        std::string String = std::to_string(cells[i + j * 9].number);
+        String = " " + String;
+        text.setString(String);
+        window.draw(text);
+      } else {
+        // random
+      }
 
-				// draw subgrid separator
-        if ((i % 3 == 0 && i != 0)) {
-					// vertical line
-          subgridSeparator.setSize({5, 1100});
-          subgridSeparator.setPosition({float(CELL_SIZE * i - 5), 0});
-          window.draw(subgridSeparator);
-        } else if ((j % 3 == 0 && j != 0)) {
-					// horizontal line
-          subgridSeparator.setSize({1100, 5});
-          subgridSeparator.setPosition({0, float(CELL_SIZE * j) + offset});
-          window.draw(subgridSeparator);
-        }
+      // draw subgrid separator
+      if ((i % 3 == 0 && i != 0)) {
+        // vertical line
+        subgridSeparator.setSize({5, 1100});
+        subgridSeparator.setPosition({float(CELL_SIZE * i - 5), 0});
+        window.draw(subgridSeparator);
+      } else if ((j % 3 == 0 && j != 0)) {
+        // horizontal line
+        subgridSeparator.setSize({1100, 5});
+        subgridSeparator.setPosition({0, float(CELL_SIZE * j) + offset});
+        window.draw(subgridSeparator);
       }
     }
   }
 }
 
 /**
+ * Displays toolbar
+ */
+void drawToolbar(sf::RenderWindow &window) {
+  pencilIcon.scale({1, 1});
+  pencilIcon.setPosition({30, 100});
+  window.draw(pencilIcon);
+}
+
+/**
  * shows time on the screen
  */
-void drawTime(sf::RenderWindow &window, sf::Time time, bool isStart) {
-  if (isStart) {
-		// show pencil icon
-    pencilIcon.scale({1, 1});
-    pencilIcon.setPosition({30, 100});
-    window.draw(pencilIcon);
+void drawTime(sf::RenderWindow &window, sf::Time time) {
+  text.setPosition({320, 0});
+  text.setFillColor(sf::Color::White);
+  std::string str = std::to_string(time.asSeconds());
+  std::string newStr = "";
 
-
-		////// show time ///////
-    text.setPosition({320, 0});
-    text.setFillColor(sf::Color::White);
-    std::string str = std::to_string(time.asSeconds());
-    std::string newStr = "";
-
-		// skip decimal part
-    for (int i = 0; i < str.length(); i++) {
-      if (str[i] == '.')
-        break;
-      newStr.append(1, str[i]);
-    }
-    text.setString(newStr);
-    window.draw(text);
+  // skip decimal part
+  for (int i = 0; i < str.length(); i++) {
+    if (str[i] == '.')
+      break;
+    newStr.append(1, str[i]);
   }
+  text.setString(newStr);
+  window.draw(text);
 }
 
 int main() {
@@ -229,12 +247,12 @@ int main() {
   clock.stop();
 
   sf::RenderWindow window = sf::RenderWindow(
-			sf::VideoMode({9 * CELL_SIZE, 9 * CELL_SIZE + offset}),
-			"Sudoku",
-			sf::Style::Titlebar | sf::Style::Close
-	);
+      sf::VideoMode({9 * CELL_SIZE, 9 * CELL_SIZE + offset}),
+      "Sudoku",
+      sf::Style::Titlebar | sf::Style::Close
+  );
 
-  window.setIcon(image.getSize(), image.getPixelsPtr());
+  window.setIcon(windowIcon.getSize(), windowIcon.getPixelsPtr());
   window.setFramerateLimit(144);
   int index = -1;
   while (window.isOpen()) {
@@ -250,7 +268,7 @@ int main() {
         clock.stop();
       }
       
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && !start) {
+      if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && !start) {
         int x = sf::Mouse::getPosition(window).x;
         int y = sf::Mouse::getPosition(window).y;
         if ((x < 400 && x > 300) && (y < 200 && y > 100)) {
@@ -260,15 +278,21 @@ int main() {
           clock.start();
           clearHighlight();
         }
+        // skip checking further inputs
+        continue;
       }
 
       if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+        
+        // clear highlights and 
         clearHighlight();
         if (index != -1)
           clearSelected(index);
         int x = sf::Mouse::getPosition(window).x;
         int y = sf::Mouse::getPosition(window).y;
-        if ((x >= 30 && y >= 100 && y <= 200 && x <= 120)) {
+
+        // clicked on toolbar
+        if (x >= 30 && y >= 100 && y <= 200 && x <= 120) {
           if (isPencil) {
             pencilIcon.setTexture(notDrawing);
             isPencil = false;
@@ -276,165 +300,59 @@ int main() {
             pencilIcon.setTexture(drawing);
             isPencil = true;
           }
+          // skip checking further inputs
+          continue;
         }
-        index = getMousPos(window);
-        if(index!=-1 && !cells[index].canBeChanged){
+        
+        // click on grid (probably)
+        index = getGridIndex(x, y);
+        if(
+          index != -1 &&
+          !cells[index].canBeChanged &&
+          cells[index].number != 0
+        ){
           highlight(cells[index].number);
-        }
-      }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Num1)) {
-        if (index != -1 && cells[index].canBeChanged) {
-          if (isPencil) {
-            pencilAppend("1", index);
-            clearSelected(index);
-            index = -1;
-          } else {
-            cells[index].number = 1;
-            highlight(1);
-            cells[index].isPencil = false;
-            cells[index].pencilNums = "";
-            clearSelected(index);
-            index = -1;
-          }
-        }
-      }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Num2)) {
-        if (index != -1 && cells[index].canBeChanged) {
-          if (isPencil) {
-            pencilAppend("2", index);
-            clearSelected(index);
-            index = -1;
-          } else {
-            cells[index].number = 2;
-            highlight(2);
-            cells[index].isPencil = false;
-            cells[index].pencilNums = "";
 
-            clearSelected(index);
-            index = -1;
-          }
+          // skip checking further inputs
+          continue;
         }
       }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Num3)) {
-        if (index != -1 && cells[index].canBeChanged) {
-          if (isPencil) {
-            pencilAppend("3", index);
-            clearSelected(index);
-            index = -1;
-          } else {
-            cells[index].number = 3;
-            cells[index].isPencil = false;
-            highlight(3);
-            cells[index].pencilNums = "";
+      
+      ////// key events /////
+      for (int i = 0; i < 9; i++) {
+        Kcode numkey = numcodes[i*2];
+        Kcode numpadkey = numcodes[i*2+1];
 
-            clearSelected(index);
-            index = -1;
+        if (
+          sf::Keyboard::isKeyPressed(numkey) ||
+          sf::Keyboard::isKeyPressed(numpadkey)
+        ) {
+          int num = i+1;
+          if (index != -1 && cells[index].canBeChanged) {
+            if (isPencil) {
+              pencilAppend(std::to_string(num), index);
+              clearSelected(index);
+              // index = -1;
+            } else {
+              cells[index].number = num;
+              highlight(num);
+              cells[index].isPencil = false;
+              cells[index].pencilNums = "";
+              clearSelected(index);
+              // keeping previous index makes it handy to delete accidential entries 
+              // index = -1;
+            }
           }
+          continue;
         }
       }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Num4)) {
-        if (index != -1 && cells[index].canBeChanged) {
-          if (isPencil) {
-            pencilAppend("4", index);
-            clearSelected(index);
-            index = -1;
-          } else {
-            cells[index].number = 4;
-            cells[index].isPencil = false;
-            highlight(4);
-            cells[index].pencilNums = "";
 
-            clearSelected(index);
-            index = -1;
-          }
-        }
-      }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Num5)) {
-        if (index != -1 && cells[index].canBeChanged) {
-          if (isPencil) {
-            pencilAppend("5", index);
-            clearSelected(index);
-            index = -1;
-          } else {
-            cells[index].number = 5;
-            clearSelected(index);
-            highlight(5);
-            cells[index].isPencil = false;
-            cells[index].pencilNums = "";
-
-            index = -1;
-          }
-        }
-      }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Num6)) {
-        if (index != -1 && cells[index].canBeChanged) {
-          if (isPencil) {
-            pencilAppend("6", index);
-            clearSelected(index);
-            index = -1;
-          } else {
-            cells[index].number = 6;
-            cells[index].isPencil = false;
-            highlight(6);
-            cells[index].pencilNums = "";
-
-            clearSelected(index);
-            index = -1;
-          }
-        }
-      }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Num7)) {
-        if (index != -1 && cells[index].canBeChanged) {
-          if (isPencil) {
-            pencilAppend("7", index);
-            clearSelected(index);
-            index = -1;
-          } else {
-            cells[index].number = 7;
-            cells[index].isPencil = false;
-            highlight(7);
-            cells[index].pencilNums = "";
-
-            clearSelected(index);
-            index = -1;
-          }
-        }
-      }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Num8)) {
-        if (index != -1 && cells[index].canBeChanged) {
-          if (isPencil) {
-            pencilAppend("8", index);
-            clearSelected(index);
-            index = -1;
-          } else {
-            cells[index].number = 8;
-            cells[index].isPencil = false;
-            cells[index].pencilNums = "";
-            highlight(8);
-
-            clearSelected(index);
-            index = -1;
-          }
-        }
-      }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Num9)) {
-        if (index != -1 && cells[index].canBeChanged) {
-          if (isPencil) {
-            pencilAppend("9", index);
-            clearSelected(index);
-            index = -1;
-          } else {
-            cells[index].number = 9;
-            cells[index].isPencil = false;
-            highlight(9);
-            cells[index].pencilNums = "";
-
-            clearSelected(index);
-            index = -1;
-          }
-        }
-      }
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Num0)) {
+      // erase keys
+      if (
+        sf::Keyboard::isKeyPressed(Kcode::Num0) || 
+        sf::Keyboard::isKeyPressed(Kcode::Backspace) ||
+        sf::Keyboard::isKeyPressed(Kcode::Delete)
+      ) {
         if (index != -1) {
           cells[index].number = 0;
           cells[index].isPencil = false;
@@ -446,10 +364,14 @@ int main() {
       }
     }
     window.clear();
-		
-    startScreen(start, window);
-    drawTime(window, elapsed1, start);
-    drawRectangles(window, isWin, start);
+    
+    if (!start) {
+      startScreen(window);
+    } else {
+      drawToolbar(window);
+      drawTime(window, elapsed1);
+      drawRectangles(window, isWin);
+    }
     window.display();
   }
 }
