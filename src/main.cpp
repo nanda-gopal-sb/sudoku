a3ed6e370538974f376b1d7165ff671d3f0291fc
Nandagopal SB <nandagopal.nsb@gmail.com>
2025-01-28 00:37:30 +0530

    fixed colour scheme and minor bug fixes, last commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,40 @@
 #include "./sudoku.cpp"
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Color.hpp>
-#include <SFML/Window/WindowEnums.hpp>
 #include <ctime>
 #include <iostream>
 #include <stdlib.h>
 #include <string>
 
-#define CELL_SIZE 80
-#define offset 240
+#define CELL_SIZE 80 // the size of each cell
+#define offset 160   // the offset for the toolbar(notice how its 80*3 )
 
 typedef sf::Keyboard::Scancode Kcode;
 
-sf::Texture texture("assests/bg.png");
-sf::Font font("assests/type.ttf");
+sf::Texture texture("assests/bg.png"); // the background textures
+sf::Font font("assests/type.ttf");     // the font used
 sf::Text text(font);
-std::vector<Cell> cells;
-sf::Time elapsed1;
-sf::Image windowIcon("assests/sudoku.png");
-sf::Texture drawing("assests/drawing.png");
-sf::Texture notDrawing("assests/not-drawing.png");
-sf::Sprite pencilIcon(notDrawing);
+std::vector<Cell> cells;                           // definition of cells is in sudoku.cpp
+sf::Time elapsed1;                                 // variable to store the time
+sf::Image windowIcon("assests/sudoku.png");        // the icon for the window
+sf::Texture drawing("assests/drawing.png");        // the pencil when its drawing
+sf::Texture notDrawing("assests/not-drawing.png"); // reverse
+sf::Sprite pencilIcon(notDrawing);                 // start the sprite with texture not drawing
 
 // for handling keypress events
 // handle both normal key and numberpad key
 Kcode numcodes[18] = {
-  Kcode::Num1, Kcode::Numpad1,
-  Kcode::Num2, Kcode::Numpad2,
-  Kcode::Num3, Kcode::Numpad3,
-  Kcode::Num4, Kcode::Numpad4,
-  Kcode::Num5, Kcode::Numpad5,
-  Kcode::Num6, Kcode::Numpad6,
-  Kcode::Num7, Kcode::Numpad7,
-  Kcode::Num8, Kcode::Numpad8,
-  Kcode::Num9, Kcode::Numpad9
-};
+    Kcode::Num1, Kcode::Numpad1,
+    Kcode::Num2, Kcode::Numpad2,
+    Kcode::Num3, Kcode::Numpad3,
+    Kcode::Num4, Kcode::Numpad4,
+    Kcode::Num5, Kcode::Numpad5,
+    Kcode::Num6, Kcode::Numpad6,
+    Kcode::Num7, Kcode::Numpad7,
+    Kcode::Num8, Kcode::Numpad8,
+    Kcode::Num9, Kcode::Numpad9};
 
-void sudokuGenerator(int k) {
+void sudokuGenerator(int k) // the definisions for these are in sudoku.cpp
+{
   Sudoku *suk = new Sudoku();
   suk->fillDiagonal(cells);
   suk->solveSudoku(cells, 0, 0);
@@ -47,27 +45,40 @@ void sudokuGenerator(int k) {
 /**
  * Checks if puzzle was solved
  */
-bool checkForVictory() {
+bool checkForVictory()
+{
   Sudoku *suk = new Sudoku();
   bool win = suk->isValid(cells);
   delete suk;
   return win;
 }
 
-void pencilAppend(std::string numToAdd, int index) {
+/*
+ * Adds the pencil marks to the string with a format.
+ */
+void pencilAppend(int numToAdd, int index)
+{
   cells[index].isPencil = true;
-  int found = cells[index].pencilNums.find(numToAdd);
-  if (found == -1) {
-    cells[index].pencilNums.append(numToAdd);
-    int len = cells[index].pencilNums.size();
-    if (len == 5 || len == 11) {
-      cells[index].pencilNums.append("\n");
-    } else {
-      cells[index].pencilNums.append(" ");
-    }
-  } else {
+  cells[index].number = 0;
+  cells[index].isHighlight = false;
+  // converts the interger into a string and checks if the string was found.
+  // it returns -1 if not found and the index of the first occurance of substring if it does.
+  int found = cells[index].pencilNums.find(std::to_string(numToAdd));
 
-    cells[index].pencilNums = cells[index].pencilNums.erase(found, 2);
+  if (found == -1)
+  {
+    /*
+    Basically the string is formatted in such a way that there are two \n chars in-between three sets of
+    empty chars so -
+    "     \n     \n     "
+    this way if I add the numbers into certain empty spaces it will apprear like an matrix.
+    makes it way easier to draw it on the screen.
+    */
+    cells[index].pencilNums[(numToAdd - 1) * 2] = std::to_string(numToAdd)[0];
+  }
+  else
+  {
+    cells[index].pencilNums[found] = ' '; // basically turns the string back into an empty space
   }
 }
 void clearSelected(int index) { cells[index].isSelected = false; }
@@ -75,23 +86,28 @@ void clearSelected(int index) { cells[index].isSelected = false; }
 /**
  * Generates a puzzle and stores it in `cells`
  */
-void fillCell() {
-  for (int a = 0; a < 9; a++) {
-    for (int b = 0; b < 9; b++) {
+void fillCell()
+{
+  for (int a = 0; a < 9; a++)
+  {
+    for (int b = 0; b < 9; b++)
+    {
       cells.push_back(Cell(b, a));
     }
   }
-  sudokuGenerator(10);
+  sudokuGenerator(40); // this decides the how many characters are removed from the solutions
 }
 
 /**
  * Returns 1D cell index from mouse's relative (x,y) position
  */
-int getGridIndex(int x, int y) {
+int getGridIndex(int x, int y)
+{
   int mouse_x = x / CELL_SIZE;
   int mouse_y = y / CELL_SIZE;
-  mouse_y = mouse_y - offset/CELL_SIZE;
-  if (mouse_y < 0) {
+  mouse_y = mouse_y - offset / CELL_SIZE;
+  if (mouse_y < 0)
+  {
     return 0;
   }
   if (cells[mouse_x + mouse_y * 9].canBeChanged)
@@ -102,8 +118,10 @@ int getGridIndex(int x, int y) {
 /**
  * Highlights a given number throughout the grid
  */
-void highlight(int num) {
-  for (int i = 0; i < 81; i++) {
+void highlight(int num)
+{
+  for (int i = 0; i < 81; i++)
+  {
     if (cells[i].number == num)
       cells[i].isHighlight = true;
   }
@@ -112,8 +130,10 @@ void highlight(int num) {
 /**
  * Clears highlighting of all cells
  */
-void clearHighlight() {
-  for (int i = 0; i < 81; i++) {
+void clearHighlight()
+{
+  for (int i = 0; i < 81; i++)
+  {
     cells[i].isHighlight = false;
   }
 }
@@ -121,9 +141,10 @@ void clearHighlight() {
 /**
  * Shows initial play game screen
  */
-void startScreen(sf::RenderWindow &window) {
+void startScreen(sf::RenderWindow &window)
+{
   text.setCharacterSize(30);
-  text.setFillColor(sf::Color::Cyan);
+  text.setFillColor(sf::Color(69, 133, 136));
   text.setString("PLAY");
   text.setPosition({300, 100});
   window.draw(text);
@@ -132,8 +153,10 @@ void startScreen(sf::RenderWindow &window) {
 /**
  * Draw board
  */
-void drawRectangles(sf::RenderWindow &window, bool isWin) {
-  if (isWin) {
+void drawRectangles(sf::RenderWindow &window, bool isWin)
+{
+  if (isWin)
+  {
     window.clear();
     int timeTaken = (elapsed1.asSeconds());
     text.setFillColor(sf::Color::White);
@@ -147,14 +170,21 @@ void drawRectangles(sf::RenderWindow &window, bool isWin) {
   sf::RectangleShape subgridSeparator({5, 1100});
   subgridSeparator.setFillColor(sf::Color::Black);
 
-  for (int i = 0; i < 9; i++) {
-    for (int j = 0; j < 9; j++) {
+  for (int i = 0; i < 9; i++)
+  {
+    for (int j = 0; j < 9; j++)
+    {
 
-      if (cells[i + j * 9].isSelected) {
-        rectangle.setFillColor(sf::Color(99, 99, 99));
-      } else if (cells[i + j * 9].isHighlight) {
-        rectangle.setFillColor(sf::Color::Magenta);
-      } else {
+      if (cells[i + j * 9].isSelected)
+      {
+        rectangle.setFillColor(sf::Color(102, 110, 120));
+      }
+      else if (cells[i + j * 9].isHighlight)
+      {
+        rectangle.setFillColor(sf::Color(218, 176, 128));
+      }
+      else
+      {
         rectangle.setFillColor(sf::Color::White);
       }
 
@@ -168,38 +198,49 @@ void drawRectangles(sf::RenderWindow &window, bool isWin) {
       // draw cell text
       text.setPosition({float(CELL_SIZE * i), float(CELL_SIZE * j) + offset});
       if (cells[i + j * 9].canBeChanged && !cells[i + j * 9].isPencil &&
-          cells[i + j * 9].number != 0) {
+          cells[i + j * 9].number != 0)
+      {
         text.setCharacterSize(64);
-        text.setFillColor(sf::Color::Black);
+        text.setFillColor(sf::Color(16, 32, 115));
         std::string String = std::to_string(cells[i + j * 9].number);
         String = " " + String;
         text.setString(String);
         window.draw(text);
-      } else if (cells[i + j * 9].isPencil) {
+      }
+      else if (cells[i + j * 9].isPencil)
+      {
         text.setCharacterSize(21);
-        text.setFillColor(sf::Color::Black);
+        text.setPosition({float(CELL_SIZE * i) + 8, float(CELL_SIZE * j) + offset});
+        text.setFillColor(sf::Color(32, 35, 70));
         text.setString(cells[i + j * 9].pencilNums);
         window.draw(text);
-      } else if (cells[i + j * 9].number != 0) {
+      }
+      else if (cells[i + j * 9].number != 0)
+      {
         text.setCharacterSize(64);
-        text.setFillColor(sf::Color(99, 62, 21));
+        text.setFillColor(sf::Color(43, 8, 4));
         std::string String = std::to_string(cells[i + j * 9].number);
         String = " " + String;
         text.setString(String);
         window.draw(text);
-      } else {
+      }
+      else
+      {
         // random
       }
 
       // draw subgrid separator
-      if ((i % 3 == 0 && i != 0)) {
+      if ((i % 3 == 0 && i != 0))
+      {
         // vertical line
         subgridSeparator.setSize({5, 1100});
-        subgridSeparator.setPosition({float(CELL_SIZE * i - 5), 0});
+        subgridSeparator.setPosition({float(CELL_SIZE * i - 5), offset});
         window.draw(subgridSeparator);
-      } else if ((j % 3 == 0 && j != 0)) {
+      }
+      else if ((j % 3 == 0 && j != 0))
+      {
         // horizontal line
-        subgridSeparator.setSize({1100, 5});
+        subgridSeparator.setSize({1100, 3});
         subgridSeparator.setPosition({0, float(CELL_SIZE * j) + offset});
         window.draw(subgridSeparator);
       }
@@ -210,32 +251,36 @@ void drawRectangles(sf::RenderWindow &window, bool isWin) {
 /**
  * Displays toolbar
  */
-void drawToolbar(sf::RenderWindow &window) {
+void drawToolbar(sf::RenderWindow &window)
+{
   pencilIcon.scale({1, 1});
-  pencilIcon.setPosition({30, 100});
+  pencilIcon.setPosition({40, 20});
   window.draw(pencilIcon);
 }
 
 /**
  * shows time on the screen
  */
-void drawTime(sf::RenderWindow &window, sf::Time time) {
-  text.setPosition({320, 0});
+void drawTime(sf::RenderWindow &window, sf::Time time)
+{
+  text.setPosition({320, 30});
   text.setFillColor(sf::Color::White);
   std::string str = std::to_string(time.asSeconds());
   std::string newStr = "";
 
   // skip decimal part
-  for (int i = 0; i < str.length(); i++) {
+  for (int i = 0; i < str.length(); i++)
+  {
     if (str[i] == '.')
       break;
     newStr.append(1, str[i]);
   }
-  text.setString(newStr);
+  text.setString(" " + newStr);
   window.draw(text);
 }
 
-int main() {
+int main()
+{
   bool isPencil = false;
   bool start = false;
   bool isWin = false;
@@ -249,29 +294,35 @@ int main() {
   sf::RenderWindow window = sf::RenderWindow(
       sf::VideoMode({9 * CELL_SIZE, 9 * CELL_SIZE + offset}),
       "Sudoku",
-      sf::Style::Titlebar | sf::Style::Close
-  );
+      sf::Style::Titlebar | sf::Style::Close);
 
   window.setIcon(windowIcon.getSize(), windowIcon.getPixelsPtr());
   window.setFramerateLimit(144);
   int index = -1;
-  while (window.isOpen()) {
+  while (window.isOpen())
+  {
     elapsed1 = clock.getElapsedTime();
-    while (const std::optional event = window.pollEvent()) {
-      if (event->is<sf::Event::Closed>()) {
+    while (const std::optional event = window.pollEvent())
+    {
+      if (event->is<sf::Event::Closed>())
+      {
         window.close();
       }
 
-      if (checkForVictory()) {
+      if (checkForVictory())
+      {
         isWin = true;
         elapsed1 = clock.getElapsedTime();
         clock.stop();
       }
-      
-      if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && !start) {
+
+      if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && !start)
+      {
+        clearHighlight();
         int x = sf::Mouse::getPosition(window).x;
         int y = sf::Mouse::getPosition(window).y;
-        if ((x < 400 && x > 300) && (y < 200 && y > 100)) {
+        if ((x < 400 && x > 300) && (y < 200 && y > 100))
+        {
           start = true;
           index = -1;
           clock.reset();
@@ -282,9 +333,10 @@ int main() {
         continue;
       }
 
-      if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
-        
-        // clear highlights and 
+      if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+      {
+
+        // clear highlights and
         clearHighlight();
         if (index != -1)
           clearSelected(index);
@@ -292,82 +344,91 @@ int main() {
         int y = sf::Mouse::getPosition(window).y;
 
         // clicked on toolbar
-        if (x >= 30 && y >= 100 && y <= 200 && x <= 120) {
-          if (isPencil) {
-            pencilIcon.setTexture(notDrawing);
-            isPencil = false;
-          } else {
+        if (x <= 140 && x > 50 && y < 100 && y > 36)
+        {
+          if (!isPencil)
+          {
             pencilIcon.setTexture(drawing);
-            isPencil = true;
+            isPencil = !isPencil;
+          }
+          else
+          {
+            isPencil = !isPencil;
+            pencilIcon.setTexture(notDrawing);
           }
           // skip checking further inputs
-          continue;
         }
-        
+
         // click on grid (probably)
         index = getGridIndex(x, y);
-        if(
-          index != -1 &&
-          !cells[index].canBeChanged &&
-          cells[index].number != 0
-        ){
+        if (
+            index != -1 &&
+            cells[index].number != 0)
+        {
           highlight(cells[index].number);
 
           // skip checking further inputs
           continue;
         }
       }
-      
+
       ////// key events /////
-      for (int i = 0; i < 9; i++) {
-        Kcode numkey = numcodes[i*2];
-        Kcode numpadkey = numcodes[i*2+1];
+      for (int i = 0; i < 9; i++)
+      {
+        Kcode numkey = numcodes[i * 2];
+        Kcode numpadkey = numcodes[i * 2 + 1];
 
         if (
-          sf::Keyboard::isKeyPressed(numkey) ||
-          sf::Keyboard::isKeyPressed(numpadkey)
-        ) {
-          int num = i+1;
-          if (index != -1 && cells[index].canBeChanged) {
-            if (isPencil) {
-              pencilAppend(std::to_string(num), index);
+            sf::Keyboard::isKeyPressed(numkey) ||
+            sf::Keyboard::isKeyPressed(numpadkey))
+        {
+          int num = i + 1;
+          if (index != -1 && cells[index].canBeChanged)
+          {
+            if (isPencil)
+            {
+              pencilAppend(num, index);
               clearSelected(index);
-              // index = -1;
-            } else {
+            }
+            else
+            {
+              cells[index].pencilNums = "     \n     \n     ";
               cells[index].number = num;
               highlight(num);
               cells[index].isPencil = false;
-              cells[index].pencilNums = "";
               clearSelected(index);
-              // keeping previous index makes it handy to delete accidential entries 
-              // index = -1;
+              // keeping previous index makes it handy to delete accidential entries
             }
           }
-          continue;
+          index = -1;
         }
       }
 
       // erase keys
       if (
-        sf::Keyboard::isKeyPressed(Kcode::Num0) || 
-        sf::Keyboard::isKeyPressed(Kcode::Backspace) ||
-        sf::Keyboard::isKeyPressed(Kcode::Delete)
-      ) {
-        if (index != -1) {
+          sf::Keyboard::isKeyPressed(Kcode::Num0) ||
+          sf::Keyboard::isKeyPressed(Kcode::Backspace) ||
+          sf::Keyboard::isKeyPressed(Kcode::Delete))
+      {
+        if (index != -1)
+        {
           cells[index].number = 0;
           cells[index].isPencil = false;
           clearHighlight();
-          cells[index].pencilNums = "";
+          cells[index].pencilNums = "     \n     \n     ";
           clearSelected(index);
           index = -1;
         }
       }
     }
-    window.clear();
-    
-    if (!start) {
+    window.clear(sf::Color(60, 56, 54));
+
+    if (!start)
+    {
       startScreen(window);
-    } else {
+    }
+    else
+    {
       drawToolbar(window);
       drawTime(window, elapsed1);
       drawRectangles(window, isWin);
--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -1,32 +1,35 @@
 #include <stdlib.h>
 #include <string>
 #include <vector>
-struct Cell {
+struct Cell
+{
   int number = 0;
   int x; // obvious
   int y; // obvious
   bool isSelected;
   bool isPencil;
   bool canBeChanged;
-  bool isHighlight; 
+  bool isHighlight;
   std::string pencilNums;
-  Cell(int x1, int y1) {
+  Cell(int x1, int y1)
+  {
     x = x1;
     y = y1;
     isPencil = false;
     isSelected = false;
     canBeChanged = false;
     isHighlight = false;
+    pencilNums = "     \n     \n     ";
   }
 };
-class Sudoku {
+class Sudoku
+{
 public:
-
   /**
    * Checks if given number is used within
    * [rowStart, colStart] to [rowStart + 2, colStart + 2]
    * can be used to check duplicate numbers in a subgrid
-   * 
+   *
    *       colStart
    *         ↓
    *  row  → o o o
@@ -34,41 +37,52 @@ public:
    *         o o o
    */
   bool unUsedInBox(std::vector<Cell> &grid, int rowStart, int colStart,
-                   int num) {
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        if (grid[(rowStart + i) + (colStart + j) * 9].number == num) {
+                   int num)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 3; j++)
+      {
+        if (grid[(rowStart + i) + (colStart + j) * 9].number == num)
+        {
           return false;
         }
       }
     }
     return true;
   }
-  
+
   // checks if number is already present in the column j
-  bool unUsedInCol(std::vector<Cell> &grid, int j, int num) {
-    for (int i = 0; i < 9; i++) {
-      if (grid[i + j * 9].number == num) {
+  bool unUsedInCol(std::vector<Cell> &grid, int j, int num)
+  {
+    for (int i = 0; i < 9; i++)
+    {
+      if (grid[i + j * 9].number == num)
+      {
         return false;
       }
     }
     return true;
   }
-  
+
   // checks if number is already present in the row i
-  bool unUsedInRow(std::vector<Cell> &grid, int i, int num) {
-    for (int j = 0; j < 9; j++) {
-      if (grid[i + j * 9].number == num) {
+  bool unUsedInRow(std::vector<Cell> &grid, int i, int num)
+  {
+    for (int j = 0; j < 9; j++)
+    {
+      if (grid[i + j * 9].number == num)
+      {
         return false;
       }
     }
     return true;
   }
-  
+
   /**
    * Returns true if number isn't already present in the same subgrid, row and col
    */
-  bool checkIfSafe(std::vector<Cell> &grid, int i, int j, int num) {
+  bool checkIfSafe(std::vector<Cell> &grid, int i, int j, int num)
+  {
     return (unUsedInRow(grid, i, num) &&
             unUsedInCol(grid, j, num) &&
             unUsedInBox(grid, i - i % 3, j - j % 3, num));
@@ -77,14 +91,17 @@ public:
   /**
    * returns whether solution is valid or not
    */
-  int isValid(std::vector<Cell> &grid) {
+  int isValid(std::vector<Cell> &grid)
+  {
 
     std::vector<std::vector<int>> rows(10, std::vector<int>(10, 0));
     std::vector<std::vector<int>> cols(10, std::vector<int>(10, 0));
     std::vector<std::vector<int>> subgrid(10, std::vector<int>(10, 0));
 
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
+    for (int i = 0; i < 9; i++)
+    {
+      for (int j = 0; j < 9; j++)
+      {
 
         // empty cells, not fully solved
         if (grid[i + j * 9].number == 0)
@@ -119,7 +136,8 @@ public:
     return true;
   }
 
-  bool solveSudoku(std::vector<Cell> &grid, int row, int col) {
+  bool solveSudoku(std::vector<Cell> &grid, int row, int col)
+  {
 
     // return true if reached last row and overflown cols
     if (row == 9 - 1 && col == 9)
@@ -128,32 +146,35 @@ public:
     /**
      * wrap col when cols overflow
      *                           currently here
-     *                            ↓ 
+     *                            ↓
      *        0 1 2  3 4 5  6 7 8 x
      * goto → x 1 2  3 4 5  6 7 8
-     * here  
-     * 
+     * here
+     *
      */
-    if (col == 9) {
+    if (col == 9)
+    {
       row++;
       col = 0;
     }
 
     /** if no number is filled, goto next column of same row
      *
-     *   currently here     
-     *   | goto here        
-     *   ↓ ↓                 
+     *   currently here
+     *   | goto here
+     *   ↓ ↓
      * 0 1 2  3 4 5  6 7 8
-     * 
+     *
      * */
     if (grid[row + col * 9].number > 0)
       return solveSudoku(grid, row, col + 1);
-    
-    // check all possible values for a cell
-    for (int num = 1; num <= 9; num++) {
 
-      if (checkIfSafe(grid, row, col, num)) {
+    // check all possible values for a cell
+    for (int num = 1; num <= 9; num++)
+    {
+
+      if (checkIfSafe(grid, row, col, num))
+      {
         // number doesn't exists in the respective subgrid, row & col
         // add num to that cell and see if it is consistent
         grid[row + col * 9].number = num;
@@ -165,16 +186,20 @@ public:
     }
     return false;
   }
-  
+
   /**
    * fill each subgrid by a random number
    * A subgrid denoted by index of top left entry
    */
-  void fillBox(std::vector<Cell> &grid, int row, int col) {
+  void fillBox(std::vector<Cell> &grid, int row, int col)
+  {
     int num;
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        do {
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 3; j++)
+      {
+        do
+        {
           num = (rand() % 9) + 1;
         } while (!unUsedInBox(grid, row, col, num));
         grid[(row + i) + (col + j) * 9].number = num;
@@ -184,9 +209,9 @@ public:
 
   /**
    * Fills diagonal subgrids with valid random arrangement of values
-   *               
+   *
    *                col index
-   *           0 1 2   3 4 5   6 7 8 
+   *           0 1 2   3 4 5   6 7 8
    *          -----------------------
    *       0   x x x | - - - | - - -
    *       1   x x x | - - - | - - -
@@ -199,10 +224,12 @@ public:
    *       6   - - - | - - - | x x x
    *       7   - - - | - - - | x x x
    *       8   - - - | - - - | x x x
-   *  
+   *
    */
-  void fillDiagonal(std::vector<Cell> &grid) {
-    for (int i = 0; i < 9; i = i + 3) {
+  void fillDiagonal(std::vector<Cell> &grid)
+  {
+    for (int i = 0; i < 9; i = i + 3)
+    {
       fillBox(grid, i, i);
     }
   }
@@ -210,13 +237,16 @@ public:
   /**
    * Removes k number of random cells
    */
-  void removeKDigits(std::vector<Cell> &grid, int k) {
-    while (k > 0) {
+  void removeKDigits(std::vector<Cell> &grid, int k)
+  {
+    while (k > 0)
+    {
 
       int cellId = rand() % 81;
       int i = cellId / 9;
       int j = cellId % 9;
-      if (grid[i + j * 9].number != 0) {
+      if (grid[i + j * 9].number != 0)
+      {
         grid[i + j * 9].number = 0;
         grid[i + j * 9].canBeChanged = true;
         k--;
